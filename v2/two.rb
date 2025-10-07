2d59d8583d5e2933e92d1ed49a5862879bfa1e76
Alex Erofeev <xnet141@gmail.com>
2025-10-07 18:40:51 +0300

    first v2

--- v2/two.rb
+++ v2/two.rb
@@ -62,17 +62,17 @@ def show_win_squares squares
 end
 
 def squares_victory array, game
-  squares_arr = [
-                 [array[0][0],array[1][0],array[2][0]], 
-                 [array[0][1],array[1][1],array[2][1]],
-                 [array[0][2],array[1][2],array[2][2]], 
-                 [array[0][0],array[0][1],array[0][2]],
-                 [array[1][0],array[1][1],array[1][2]], 
-                 [array[2][0],array[2][1],array[2][2]],
-                 [array[0][0],array[1][1],array[2][2]], 
-                 [array[0][2],array[1][1],array[2][0]],
-                ]
-  squares_arr.each do |squares| 
+  [
+    [array[0][0],array[1][0],array[2][0]], 
+    [array[0][1],array[1][1],array[2][1]],
+    [array[0][2],array[1][2],array[2][2]], 
+    [array[0][0],array[0][1],array[0][2]],
+    [array[1][0],array[1][1],array[1][2]], 
+    [array[2][0],array[2][1],array[2][2]],
+    [array[0][0],array[1][1],array[2][2]], 
+    [array[0][2],array[1][1],array[2][0]],
+  ]
+  .each do |squares| 
     if !squares.include?(nil)
       show_win_squares(squares) 
       @game = "stop"
