fd51779fcc0e20689981528e353f778f5b15c60a
Alex Erofeev <xnet141@gmail.com>
2025-10-09 14:46:44 +0300

    init v4

---
+++ v4/game.rb
@@ -0,0 +1,40 @@
+require 'ruby2d'
+require_relative 'lib/player'
+require_relative 'lib/logic'
+
+set title: "Hello!"
+
+GRID_SIZE = 200
+WIDTH = 3 
+HEIGHT = 3
+GRID_COLOR = Color.new('#222222')
+BLOCK_COLOR = Color.new(['orange', 'yellow', 'green'].sample)
+
+set width: WIDTH * GRID_SIZE
+set height: HEIGHT * GRID_SIZE
+
+(0..Window.width).step(GRID_SIZE).each do |x|
+  Line.new(x1: x, x2: x, y1: 0, y2: Window.height, width: 2, color: GRID_COLOR, z: 1) # z - слой
+end
+
+(0..Window.height).step(GRID_SIZE).each do |y|
+  Line.new(y1: y, y2: y, x1: 0, x2: Window.width, width: 2, color: GRID_COLOR, z: 1)
+end
+
+player = Player.new
+logic = Logic.new
+
+on :mouse_down do |event| 
+  case event.button
+  when :left
+    if logic.game_start # == true
+      player.circle event.x, event.y
+      logic.logic event.x, event.y
+    end  
+  when :right
+    close
+  end
+end
+
+
+show
